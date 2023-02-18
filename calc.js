4ad655c48e20e6571317f15f7595595c82c110ab
Satyam Rai <satyamrai0510@gmail.com>
2023-02-18 12:32:02 +0530

    :heavy_plus_sign:

--- calc.js
+++ calc.js
@@ -48,3 +48,48 @@ console.log(
       }
     });
   }
+  
+  document.addEventListener("keydown", function (event) {
+    if (event.shiftKey == 57) {
+      event.key = "(";
+    } else if (event.shiftKey == 48) {
+      event.key = ")";
+    } else if (event.shiftKey == 53) {
+      event.key = "%";
+    }
+    if (event.keyCode == 88) {
+      screenValue += "*";
+      screen.value = screenValue;
+    }
+    if (
+      event.key <= 9 ||
+      event.key == "+" ||
+      event.key == "-" ||
+      event.key == "*" ||
+      event.key == "." ||
+      event.key == "/" ||
+      event.key == "%" ||
+      event.key == "(" ||
+      event.key == ")"
+    ) {
+      screenValue += event.key;
+      screen.value = screenValue;
+    }
+    if (event.keyCode == 13 || event.keyCode == 187) {
+      checkForBracketMulti();
+    } else if (event.keyCode == 46) {
+      screenValue = "";
+      screen.value = screenValue;
+      console.clear();
+    } else if (event.keyCode == 8) {
+      screenValue = screenValue.slice(0, -1);
+      screen.value = screenValue;
+    } else if (event.keyCode == 67) {
+      screenValue = "";
+      screen.value = screenValue;
+      console.clear();
+    } else if (event.keyCode == 82) {
+      location.reload();
+    }
+  });
+  
