6bd7a30931e9967143ca4f421e69ccece4645ebb
Satyam Rai <satyamrai0510@gmail.com>
2023-02-18 12:28:37 +0530

    :heavy_plus_sign:

--- calc.js
+++ calc.js
@@ -5,3 +5,46 @@ console.log(
   function isNumber(char) {
     return /^\d$/.test(char);
   }
+  
+  document.getElementById("answer").readOnly = true;
+  let screen = document.getElementById("answer");
+  buttons = document.querySelectorAll("button");
+  let screenValue = "";
+  let maxItems = 6;
+  for (item of buttons) {
+    item.addEventListener("click", (e) => {
+      buttonText = e.target.innerText;
+      if (buttonText == "X") {
+        if (flag == 1) {
+          flag = 0;
+        }
+        buttonText = "*";
+        screenValue += buttonText;
+        screen.value = screenValue;
+      } else if (buttonText == "C") {
+        if (flag == 1) {
+          flag = 0;
+        }
+        screenValue = "";
+        screen.value = screenValue;
+      } else if (buttonText == "=") {
+        checkForBracketMulti();
+
+      } else if (isNumber(buttonText)) {
+        if (flag == 1) {
+          screenValue = buttonText;
+          flag = 0;
+        } else {
+          screenValue += buttonText;
+        }
+        screen.value = screenValue;
+        console.log("im an operator");
+      } else {
+        if (flag == 1) {
+          flag = 0;
+        }
+        screenValue = screen.value + buttonText;
+        screen.value = screenValue;
+      }
+    });
+  }
